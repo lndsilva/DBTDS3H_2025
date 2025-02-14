57112d8136bd4a7f1e1b71afb5d0852045658b10
lndsilva <lndsilva@hotmail.com>
2025-02-14 20:23:44 -0300

    banco de dados padaria tabelas

--- dbpadaria.sql
+++ dbpadaria.sql
@@ -1,28 +1,59 @@
 -- apagando banco de dados se existir
 drop database dbpadaria;
--- criando banco de dados
+-- criando banco de dados 
 create database dbpadaria;
 -- acessar o banco de dados
 use dbpadaria;
 --criando as tabelas no banco de dados
-create table tbUsuarios(
-codUsu int not null auto_increment,
-nome varchar(50) not null,
-senha varchar(12) not null,
-primary key(codUsu)
-);
+
+create table tbGenero(
+codGen int not null auto_increment,
+nome varchar(100),
+primary key(codGen));
+
 create table tbFuncionarios(
 codFunc int not null auto_increment,
 nome varchar(100),
 email varchar(100),
 telCel char(10),
 dataNasc datetime,
-salario decimal(9,2),
-sexo char(1),
-primary key(codFunc)
+salario decimal(9,2) default 0 check(salario >= 0),
+sexo char(1) default "F" check(sexo in("F","M")),
+codGen int not null,
+primary key(codFunc),
+foreign key(codGen)references tbGenero(codGen));
+
+create table tbUsuarios(
+codUsu int not null auto_increment,
+nome varchar(50) not null,
+senha varchar(12) not null,
+codFunc int not null,
+primary key(codUsu),
+foreign key(codFunc)references tbFuncionarios(codFunc)
 );
+
+create table tbFornecedores(
+codForn int not null auto_increment,
+nome varchar(100),
+email varchar(100),
+telCel char(10),
+primary key(codForn));
+
+create table tbProdutos(
+codProd int not null auto_increment,
+descricao varchar(100),
+quantidade int,
+preco decimal(9,2),
+dataVal datetime,
+lote char(10),
+codForn int not null,
+primary key(codProd),
+foreign key(codForn)references tbFornecedores(codForn));
+
 -- visualizando as tabelas
 show tables;
 -- visualizando a estrutura das tabelas
+desc tbGenero;
 desc tbUsuarios;
+desc tbFornecedores;
 desc tbFuncionarios;
